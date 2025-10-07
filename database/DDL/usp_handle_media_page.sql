a016cbc01b19f9a3ace7d6ab6721288a766ee856
NurmuhammadTukhtayev <nick02940@gmail.com>
2025-10-07 23:58:30 +0500

    change language to russian, remove unnecessary images

--- database/DDL/usp_handle_media_page.sql
+++ database/DDL/usp_handle_media_page.sql
@@ -5,8 +5,8 @@ DELIMITER //
 CREATE PROCEDURE usp_handle_media_page (IN p_category VARCHAR(255))
 BEGIN
     IF p_category = 'videos' THEN
-        SELECT * FROM media_videos
-    ELSE IF p_category = 'presentations' THEN
+        SELECT * FROM media_videos;
+    ELSEIF p_category = 'presentations' THEN
         SELECT * FROM media_files;
     END IF;
 END //
