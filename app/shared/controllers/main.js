b98f8bca3a6f45245e497c5faef9da49491cb411
NurmuhammadTukhtayev <nick02940@gmail.com>
2025-10-10 21:50:44 +0500

    small bugfix on main page

--- app/shared/controllers/main.js
+++ app/shared/controllers/main.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
     const currentYear = new Date().getFullYear(); 
 
     let learning_material_categories = await query("SELECT * FROM smart_path.learning_material_categories;");
-    let blogs = await query("SELECT id, title, substring(content, 1, 300) as content, author, image_url, DATE_FORMAT(published_at, '%M %e, %Y') AS published_on FROM smart_path.blog_posts order by published_at desc limit 3;");
+    let blogs = await query("SELECT id, title, substring(content, 1, 300) as content, author, image_url, DATE_FORMAT(created_at, '%M %e, %Y') AS published_on FROM smart_path.blog_posts order by created_at desc limit 3;");
 
     res.render('./shared/index', { copyrightYear: currentYear, learning_material_categories, blogs });
 };
