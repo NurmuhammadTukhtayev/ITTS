c0eb7b83536b5d68a0068c915f86b3039c6a0a2d
NurmuhammadTukhtayev <nick02940@gmail.com>
2025-10-24 03:23:39 +0500

    add download for assignment

--- app/shared/controllers/assignment.js
+++ app/shared/controllers/assignment.js
@@ -1,4 +1,6 @@
 const { query } = require('../../../database/connction/query');
+const path = require('path');
+const fs = require('fs');
 
 // get about
 const get_assignment = async (req, res, next) => {
@@ -17,4 +19,30 @@ const get_assignment = async (req, res, next) => {
     }
 }
 
-module.exports = { get_assignment };
+const download_assignment = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    // Fetch material info from DB by ID
+    const [material] = await query('SELECT file_path FROM assignments WHERE file_path = ?', [id]);
+
+    if (!material || !material.file_path) 
+      return res.render('./shared/error', { 
+          copyrightYear: res.locals.copyrightYear, 
+          learning_material_categories: res.locals.learning_material_categories,
+      });
+
+    const filePath = path.join(__dirname, '../../../public/uploads/docs', material.file_path);
+    if (!fs.existsSync(filePath)) 
+      return res.render('./shared/error', { 
+        copyrightYear: res.locals.copyrightYear, 
+        learning_material_categories: res.locals.learning_material_categories,
+      });
+
+    res.download(filePath); // this sets headers for download
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+module.exports = { get_assignment, download_assignment };
--- app/shared/routes/assignment.js
+++ app/shared/routes/assignment.js
@@ -5,5 +5,8 @@ const controller = require('../controllers/assignment');
 // get assignment
 app.get('/', controller.get_assignment);
 
+// dowload assignment
+app.get('/:id', controller.download_assignment);
+
 
 module.exports = app;
